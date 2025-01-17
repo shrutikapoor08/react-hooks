a53a0ca6e14d304de73ba5f2826b7660d5c3cc21
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-01-17 11:58:56 -0800

    Adding useRef

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -2,7 +2,10 @@ import { forwardRef } from "react";
 
 const Slide = forwardRef(function Slide({ image }, ref) {
   return (
-    <div className="slide" tabIndex={0} ref={ref}>
+    <div className="slide" tabIndex={0}>
+      <a ref={ref} href="#">
+        {image?.title}
+      </a>
       <img src={image.images[0]} alt={image?.title} />
     </div>
   );
