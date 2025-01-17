ddda5b03834cc4fb8257be219c7bd81825c6a81d
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-01-16 22:17:10 -0800

    WIP

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,11 +1,11 @@
 import { forwardRef } from "react";
-export default forwardRef(
-  props,
-  ref
-)(function Slide({ image }, ref) {
+
+const Slide = forwardRef(function Slide({ image }, ref) {
   return (
     <div className="slide" tabIndex={0} ref={ref}>
       <img src={image.images[0]} alt={image?.title} />
     </div>
   );
 });
+
+export default Slide;
